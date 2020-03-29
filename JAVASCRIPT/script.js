b65b48b5a79eb816af838354d62e6245bb296778
Toyin <oluwatoyinogundare@gmail.com>
2020-03-28 20:10:56 -0600

    century

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,13 +1,12 @@
 function myFunction()
 {
 var x = document.getElementById("year").value;
-var y = document.getElementById("century").value;
+/*var y = document.getElementById("century");*/
 
 if (x > 1000 && x < 1101)
 {
-    document.getElementById("century").innerHTML = "This is the 11th a  century";
+    document.getElementById("demo").innerHTML = "This is a new value";
 }
-
 else
 {
     alert("not a century");
